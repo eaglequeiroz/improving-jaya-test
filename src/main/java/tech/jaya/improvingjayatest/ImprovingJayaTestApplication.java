fe7513676f39f34e7c7b5290f77dec47b091490d
eaglequeiroz <igorfqueirozz@gmail.com>
2019-05-27 19:33:49 -0300

    Initial commit

---
+++ src/main/java/tech/jaya/improvingjayatest/ImprovingJayaTestApplication.java
@@ -0,0 +1,13 @@
+package tech.jaya.improvingjayatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprovingJayaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImprovingJayaTestApplication.class, args);
+	}
+
+}
