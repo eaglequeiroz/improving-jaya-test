e06d82f5936ad994a05e28675a412df8a65e8f93
eaglequeiroz <igorfqueirozz@gmail.com>
2019-05-30 18:41:22 -0300

    Implementing the API to receive a POST request (working in progress)

--- src/main/java/tech/jaya/improvingjayatest/controllers/PayloadController.java
+++ src/main/java/tech/jaya/improvingjayatest/controllers/PayloadController.java
@@ -1,5 +1,7 @@
 package tech.jaya.improvingjayatest.controllers;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import tech.jaya.improvingjayatest.models.Payload;
 import tech.jaya.improvingjayatest.services.PayloadService;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -26,11 +29,12 @@ public class PayloadController {
         return payloadService.listAll();
     }
 
-    @PostMapping(consumes = {"application/x-www-form-urlencoded"} )
-    public Payload create(String payload) {
-        //Testing to see what's being sent as payload
-        System.out.println(payload);
-        Payload  hh = new Payload();
-        return payloadService.create(hh);
+    @PostMapping(consumes = {"application/x-www-form-urlencoded"})
+    public Payload create(String payload) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+        Payload serializedObject = objectMapper.readValue(payload, Payload.class);
+        return payloadService.create(serializedObject);
     }
 }
